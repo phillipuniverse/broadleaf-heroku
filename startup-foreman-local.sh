df284e6320b7a77a24f47bb8502750567c1067c8
Phillip Verheyden <pverheyden@gmail.com>
2012-04-17 23:32:20 -0500

    Added ability to run the application locally with foreman start --procfile=Procfile-local

---
+++ startup-foreman-local.sh
@@ -0,0 +1,16 @@
+#point to the correct configuration and webapp
+CATALINA_BASE=`pwd`/site-war/target/cargo/configurations/tomcat6x
+export CATALINA_BASE
+
+#copy over the Heroku config files
+cp ./site-war/heroku-server.xml ./site-war/target/cargo/configurations/tomcat6x/conf/server.xml
+
+#make the Tomcat scripts executable
+chmod a+x ./site-war/target/cargo/installs/apache-tomcat-6.0.18/apache-tomcat-6.0.18/bin/*.sh
+
+#set the correct port and database settings
+JAVA_OPTS="$JAVA_OPTS -XX:MaxPermSize=256M -Xmx512M -Dhttp.port=8080 -DDATABASE_URL=postgres://postgres:password@localhost/postgres/broadleaf"
+export JAVA_OPTS
+
+#start Tomcat
+./site-war/target/cargo/installs/apache-tomcat-6.0.18/apache-tomcat-6.0.18/bin/catalina.sh run
